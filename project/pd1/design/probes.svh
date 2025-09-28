fab0f810f580b06472884f48bc5e88023988265d
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-09-27 21:37:40 -0400

    Update probes.svh

--- project/pd1/design/probes.svh
+++ project/pd1/design/probes.svh
@@ -1,14 +1,29 @@
-// ----  Probes  ----
-//`define PROBE_ADDR      // ??
-//`define PROBE_DATA_IN   // ??
-//`define PROBE_DATA_OUT  // ??
-//`define PROBE_READ_EN   // ??
-//`define PROBE_WRITE_EN  // ??
+`include "probes.svh"
 
-//`define PROBE_F_PC // ??
-//`define PROBE_F_INSN // ??
-// ----  Probes  ----
+// Wrapper for our design defined in code/
+// This is what the testbench connects to.
+module design_wrapper (
+    input  logic clk,
+    input  logic reset,
 
-// ----  Top module  ----
-`define TOP_MODULE  pd1
-// ----  Top module  ----
+    // Expose memory interface to testbench
+    output logic [`DWIDTH-1:0] mem_addr,
+    output logic [`DWIDTH-1:0] mem_data_out,
+    input  logic [`DWIDTH-1:0] mem_data_in,
+    output logic               mem_read_en,
+    output logic               mem_write_en
+);
+
+    // Instantiate top-level design (pd1.sv)
+    `TOP_MODULE core (
+        .clk        (clk),
+        .rst        (reset),
+
+        .mem_addr   (mem_addr),
+        .mem_data_in(mem_data_in),
+        .mem_data_out(mem_data_out),
+        .mem_read_en(mem_read_en),
+        .mem_write_en(mem_write_en)
+    );
+
+endmodule
