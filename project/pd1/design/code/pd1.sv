a65c2c970e0a8dedcd0e3c7c672681adce16ee61
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-10-14 14:50:19 -0400

    Update pd1.sv

--- project/pd1/design/code/pd1.sv
+++ project/pd1/design/code/pd1.sv
@@ -15,49 +15,48 @@ module pd1 #(
     input logic reset
 );
 
+//Fetch Probes
+logic [AWIDTH-1:0] f_pc;
+logic [DWIDTH-1:0] f_inst;
+
+//Memory Probes
+logic [AWIDTH-1:0] addr_i_top;
+logic [DWIDTH-1:0] data_i;    
+logic [DWIDTH-1:0] data_o;    
+logic read_en = 1'b1;        //set read enable signal for reading 
+logic write_en = 1'b0;       //disable write enable signal for reading 
+
+
  /*
   * Instantiate other submodules and
   * probes. To be filled by student...
   *
   */
-	
-    // imemory signals
-    logic [DWIDTH - 1:0] addr_i;
-    logic [DWIDTH - 1:0] data_i;
-    logic write_en;
-    logic read_en;
-       
-    // Fetch signals
-    logic [DWIDTH - 1:0] f_pc;
-    logic [DWIDTH - 1:0] f_insn;
-       
-    memory #(
-        .AWIDTH(32),
-        .DWIDTH(32),
-        .BASE_ADDR(32'h01000000)
-       ) memory1 (
-        .clk(clk),
-        .rst(reset),
-        .addr_i(f_pc),
-        .data_i(data_i),
-        .read_en_i(read_en),
-        .write_en_i(write_en),
-        .data_o(f_insn)
-   );
- 
-    assign read_en = 1'b1;
-    assign write_en = 1'b0;
 
-    // Fetch
-    fetch #(
-        .AWIDTH(32),
-        .DWIDTH(32),
-        .BASEADDR(32'h01000000)
-    ) fetch1 (
-        .clk(clk),
-        .rst(reset),
-        .pc_o(f_pc),            
-        .insn_o(f_insn)         
+  fetch #(
+    .DWIDTH (DWIDTH),
+    .AWIDTH (AWIDTH),
+    .BASEADDR (32'h0100_0000)
+    ) fetch1(
+      .clk (clk),
+      .rst (reset),
+      .pc_o(f_pc),
+      .insn_o(f_inst)
+      //.insn_o(data_o)
     );
 
+    memory #(
+      .AWIDTH (AWIDTH),
+      .DWIDTH (DWIDTH),
+      .BASE_ADDR(32'h0100_0000)
+      ) memory1(
+        .clk (clk),
+        .rst (reset),
+        .addr_i (f_pc), 
+        .data_i (data_i),
+        .read_en_i (read_en),
+        .write_en_i (write_en),
+        .data_o (f_inst) //used to be data_o
+    );
 endmodule : pd1
+
