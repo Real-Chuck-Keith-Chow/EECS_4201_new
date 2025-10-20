836df406214adb1fa35fe697fe68ee0c62cc1bb0
Cheuk Fung Keith (Chuck) Chow <rosarollins069@gmail.com>
2025-10-20 19:52:10 -0400

    Update probes.svh

--- project/pd3/design/probes.svh
+++ project/pd3/design/probes.svh
@@ -1,30 +1,15 @@
 // ----  Probes  ----
-//`define PROBE_F_PC // ??
-//`define PROBE_F_INSN // ??
+`define PROBE_ADDR      f_pc      //connection where pc_o and addr_i meet
+`define PROBE_DATA_IN   data_i    //input for data in memory
+`define PROBE_DATA_OUT  data_o    //output for data in memory
+`define PROBE_READ_EN   read_en   
+`define PROBE_WRITE_EN  write_en  
 
-//`define PROBE_D_PC // ??
-//`define PROBE_D_OPCODE // ??
-//`define PROBE_D_RD // ??
-//`define PROBE_D_FUNCT3 // ??
-//`define PROBE_D_RS1 // ??
-//`define PROBE_D_RS2 // ??
-//`define PROBE_D_FUNCT7 // ??
-//`define PROBE_D_IMM // ??
-//`define PROBE_D_SHAMT // ??
+`define PROBE_F_PC      f_pc    //input memory address/PC 
+`define PROBE_F_INSN    f_inst  //fetch_insn_o 
 
-//`define PROBE_R_WRITE_ENABLE      // ??
-//`define PROBE_R_WRITE_DESTINATION // ??
-//`define PROBE_R_WRITE_DATA        // ??
-//`define PROBE_R_READ_RS1          // ??
-//`define PROBE_R_READ_RS2          // ??
-//`define PROBE_R_READ_RS1_DATA     // ??
-//`define PROBE_R_READ_RS2_DATA     // ??
-
-//`define PROBE_E_PC                // ??
-//`define PROBE_E_ALU_RES           // ??
-//`define PROBE_E_BR_TAKEN          // ??
 // ----  Probes  ----
 
 // ----  Top module  ----
-`define TOP_MODULE  pd3
+`define TOP_MODULE  pd1
 // ----  Top module  ----
