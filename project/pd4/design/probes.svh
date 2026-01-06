8aa5ef7978f3dfd767fea7cd864b7a4ec3b99c2a
Cheuk Fung Keith (Chuck) Chow <rosarollins069@gmail.com>
2026-01-05 19:20:34 -0500

    Probes name changing to fix naming debug

--- project/pd4/design/probes.svh
+++ project/pd4/design/probes.svh
@@ -1,39 +1,42 @@
+// ----  Probes  ----
+`define PROBE_F_PC               fetch_pc
+`define PROBE_F_INSN             fetch_insn
 
-`define PROBE_F_PC           PROBE_F_PC_q
-`define PROBE_F_INSN         PROBE_F_INSN_q
+`define PROBE_D_PC               decode_pc
+`define PROBE_D_OPCODE           decode_opcode
+`define PROBE_D_RD               decode_rd
+`define PROBE_D_FUNCT3           decode_funct3
+`define PROBE_D_RS1              decode_rs1
+`define PROBE_D_RS2              decode_rs2
+`define PROBE_D_FUNCT7           decode_funct7
+`define PROBE_D_IMM              decode_imm
+`define PROBE_D_SHAMT            decode_shamt
 
-`define PROBE_D_PC           PROBE_D_PC_q
-`define PROBE_D_OPCODE       D_opcode
-`define PROBE_D_RD           D_rd
-`define PROBE_D_FUNCT3       D_funct3
-`define PROBE_D_RS1          D_rs1
-`define PROBE_D_RS2          D_rs2
-`define PROBE_D_FUNCT7       D_funct7
-`define PROBE_D_IMM          PROBE_D_IMM_q
-`define PROBE_D_SHAMT        D_shamt
+`define PROBE_R_WRITE_ENABLE      regwren
+`define PROBE_R_WRITE_DESTINATION decode_rd
+`define PROBE_R_WRITE_DATA        writeback_data
+`define PROBE_R_READ_RS1          decode_rs1
+`define PROBE_R_READ_RS2          decode_rs2
+`define PROBE_R_READ_RS1_DATA     rf_rs1_data
+`define PROBE_R_READ_RS2_DATA     rf_rs2_data
 
-`define PROBE_R_WRITE_ENABLE      PROBE_R_WE_q
-`define PROBE_R_WRITE_DESTINATION PROBE_R_RD_q
-`define PROBE_R_WRITE_DATA        PROBE_R_WD_q
-`define PROBE_R_READ_RS1          PROBE_R_RS1_q
-`define PROBE_R_READ_RS2          PROBE_R_RS2_q
-`define PROBE_R_READ_RS1_DATA     PROBE_R_RS1D_q
-`define PROBE_R_READ_RS2_DATA     PROBE_R_RS2D_q
+`define PROBE_E_PC                decode_pc
+`define PROBE_E_ALU_RES           alu_result
+`define PROBE_E_BR_TAKEN          branch_taken
 
-`define PROBE_E_PC                PROBE_E_PC_q
-`define PROBE_E_ALU_RES           PROBE_E_ALU_q
-`define PROBE_E_BR_TAKEN          PROBE_E_TAKEN_q
+`define PROBE_M_PC                decode_pc
+`define PROBE_M_ADDRESS           mem_address
+`define PROBE_M_SIZE_ENCODED      mem_size
+`define PROBE_M_DATA              mem_stage_data
 
-`define PROBE_M_PC                PROBE_M_PC_q
-`define PROBE_M_ADDRESS           PROBE_M_ADDR_q
-`define PROBE_M_SIZE_ENCODED      PROBE_M_SIZE_q
-`define PROBE_M_DATA              PROBE_M_DATA_q
+`define PROBE_W_PC                decode_pc
+`define PROBE_W_ENABLE            regwren
+`define PROBE_W_DESTINATION       decode_rd
+`define PROBE_W_DATA              writeback_data
 
-`define PROBE_W_PC                PROBE_W_PC_q
-`define PROBE_W_ENABLE            PROBE_W_EN_q
-`define PROBE_W_DESTINATION       PROBE_W_DEST_q
-`define PROBE_W_DATA              PROBE_W_DATA_q
+// ----  Probes  ----
 
 // ----  Top module  ----
-`define TOP_MODULE  pd4
+`define TOP_MODULE  pd4 
 // ----  Top module  ----
+
